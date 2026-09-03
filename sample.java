bdf788ead9c570aa9b0aa62635956aecb6a5d9bc
sriya sahoo <sriyasahoo388@gmail.com>
2026-09-03 15:16:19 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,6 +3,6 @@ class java
 	public static void main(String[] args) 
 	{
 		System.out.println("hello from jenkins");
-		System.out.println("welcome to jenkins");
+		System.out.println("welcome to jenkins!!!!!!");
 	}
 }
